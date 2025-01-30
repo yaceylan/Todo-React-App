58af5d5c07841934d6621dc8c91f4d7541752588
Yasemin Ceylan <yaceybln@gmail.com>
2025-01-30 15:10:29 +0100

    add todoItem component

---
+++ todo-react-app/src/components/todo-item.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function TodoItem({ todo, onTodoToggle, onTodoDelete }) { // "props" (Eigenschaften), die an die Komponente übergeben werden. todo enthält die Daten für ein einzelnes Todo-Element
+  return (
+    <li>
+      <input
+        type="checkbox" // Kontrollkästchen um Erledigtes zu markieren
+        checked={todo.completed}
+        onChange={() => onTodoToggle(todo.id)} // wenn sich Zustand des Kontrollkästchens ändert
+      />
+      <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>  
+        {todo.text} 
+      </span>
+      <button onClick={() => onTodoDelete(todo.id)}>Löschen</button>
+    </li>
+  );
+}
+
+export default TodoItem;
